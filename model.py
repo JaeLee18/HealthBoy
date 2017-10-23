0f08406029f9363d97c030ca449895fba5c7e7ad
JaeLee18 <lee2161@purdue.edu>
2017-10-23 16:10:20 -0400

    first commit

---
+++ model.py
@@ -0,0 +1,15 @@
+
+# Create User model
+from app import app, db
+# db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    score = db.Column(db.String(120))
+
+    def __init__(self, username, score):
+        self.username = username
+        self.score = score
+
+    def __repr__(self):
+        return '[username %r]' % self.username
